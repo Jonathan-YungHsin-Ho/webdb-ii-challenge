a10507d9adfa0e023986d4187bf6df3b99d7ccb2
Jonathan Ho <johnnyho82@gmail.com>
2019-10-08 16:00:10 -0400

    Add validateCar custom middleware

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -1,7 +1,38 @@
 const express = require('express');
 
-const Cars = require('../data/dbConfig');
+const db = require('../data/dbConfig');
 
 const router = express.Router();
+
+// **********************************************************************
+
+// CRUD Endpoints
+
+// GET /api/cars endpoint to READ/RETRIEVE cars -
+
+// GET /api/cars/:id endpoint to READ/RETRIEVE car by ID -
+
+// POST /api/cars endpoint to CREATE a car -
+
+// **********************************************************************
+
+// Custom Middleware
+
+// Validate body on create/update car request - NEEDS TESTING
+function validateCar(req, res, next) {
+  if (!Object.keys(req.body).length) {
+    res.status(400).json({ message: 'Missing car data!' });
+  } else if (!req.body.VIN) {
+    res.status(400).json({ message: 'Missing required "VIN" field!' });
+  } else if (!req.body.make) {
+    res.status(400).json({ message: 'Missing required "make" field!' });
+  } else if (!req.body.model) {
+    res.status(400).json({ message: 'Missing required "model" field!' });
+  } else {
+    next();
+  }
+}
+
+// **********************************************************************
 
 module.exports = router;
