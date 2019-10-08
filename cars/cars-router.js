3c20f17e6de5f0214138f4c2ea4f7eac2036660c
Jonathan Ho <johnnyho82@gmail.com>
2019-10-08 16:04:53 -0400

    Add GET /api/cars endpoint

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -9,10 +9,22 @@ const router = express.Router();
 // CRUD Endpoints
 
 // GET /api/cars endpoint to READ/RETRIEVE cars -
+router.get('/', (req, res) => {
+  db('cars')
+    .then(cars => {
+      res.status(200).json(cars);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'Error retrieving the cars' });
+    });
+});
 
 // GET /api/cars/:id endpoint to READ/RETRIEVE car by ID -
+router.get('/:id', (req, res) => {});
 
 // POST /api/cars endpoint to CREATE a car -
+router.post('/', (req, res) => {});
 
 // **********************************************************************
 
